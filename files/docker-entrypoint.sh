7e4e935b56e3ec3cef8ac614431e7d12935405bd
Nemental <15136847+Nemental@users.noreply.github.com>
2023-11-30 16:08:55 +0100

    feat: docker entrypoint build script

---
+++ files/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "\n===== CLONING NEOVIM REPOSITORY ====="
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout stable
+
+echo "\n===== BUILDING NEOVIM ====="
+make CMAKE_BUILD_TYPE=${BUILD_TYPE:-Release}
+
+echo "\n===== INSTALLING NEOVIM ====="
+make CMAKE_INSTALL_PREFIX=$HOME/nvim install
