ecadfe436ace6fe0e6bcf9044589dc7f3715645c
Nemental <15136847+Nemental@users.noreply.github.com>
2024-06-18 13:45:15 +0200

    chore: better log output

--- roles/build/files/docker-entrypoint.sh
+++ roles/build/files/docker-entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+echo "\n====================================="
 echo "\n===== CLONING NEOVIM REPOSITORY ====="
+echo "\n====================================="
 git clone https://github.com/neovim/neovim.git ~/neovim
 cd ~/neovim
 git checkout stable
 
+echo "\n==========================="
 echo "\n===== BUILDING NEOVIM ====="
+echo "\n==========================="
 make CMAKE_BUILD_TYPE=${BUILD_TYPE:-Release}
 
+echo "\n============================="
 echo "\n===== INSTALLING NEOVIM ====="
+echo "\n============================="
 make CMAKE_INSTALL_PREFIX=~/nvim install
